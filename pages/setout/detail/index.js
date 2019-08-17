a7513d9c95a0f6e75c8cb157a819fb314cdda37e
moz <nn880328@126.com>
2019-08-17 13:45:16 +0800

    实现详情页--选项卡

--- pages/setout/detail/index.js
+++ pages/setout/detail/index.js
@@ -5,62 +5,75 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    advisorMsg: "<p>在天与地的圣所，</p><p>在天与地的圣所，</p><p>在天与地的圣所，</p><p>在天与地的圣所，</p><p>在天与地的圣所，</p><p>在天与地的圣所，</p>",
+    currentTab: 0, //预设当前项的值
+    scrollLeft: 0, //tab标题的滚动条位置
   },
-
+  // 点击标题切换当前页时改变样式
+  swichNav(e) {
+    var cur = e.currentTarget.dataset.current;
+    if (this.data.currentTab == cur) {
+      return false;
+    } else {
+      this.setData({
+        currentTab: cur
+      })
+    }
+    // 滚动至对应区域
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
